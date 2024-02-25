5cfde3d7469c43c47bca1ad6ae57edad98621b78
Adam <adamghebrezgi@gmail.com>
2024-02-25 11:05:46 +0100

    1 bit full adder

---
+++ ex_adder/CMakeFiles/ex_adder.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex_adder.dir/main.cpp.o"
+  "CMakeFiles/ex_adder.dir/main.cpp.o.d"
+  "ex_adder"
+  "ex_adder.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex_adder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- ex_adder/main.cpp
+++ ex_adder/main.cpp
@@ -1,3 +1,85 @@
 //
 // Created by adam on 22.02.24.
 //
+// simple 2 bit full-adder
+
+#include <systemc.h>
+#include <iostream>
+
+SC_MODULE(adder) {
+    sc_in<bool> a;
+    sc_in<bool> b;
+    sc_in<bool> cin;
+    sc_out<bool> sum;
+    sc_out<bool> co;
+
+    SC_CTOR(adder) {
+        SC_METHOD(process);
+        sensitive << a << b << cin;
+    }
+
+    void process() {
+        sum.write ( a.read() xor b.read() xor cin.read());
+        co.write ((a.read() && b.read()) || ((a.read() xor b.read()) and cin.read()));
+
+    }
+};
+
+SC_MODULE(toplevel) {
+    sc_signal<bool> A;
+    sc_signal<bool> B;
+    sc_signal<bool> CIN;
+    sc_signal<bool> S;
+    sc_signal<bool> CO;
+
+    adder fa;
+
+    SC_CTOR(toplevel): fa("fa") {
+        SC_THREAD(stimuli);
+
+        //do binding
+        fa.a.bind(A);
+        fa.b.bind(B);
+        fa.cin.bind(CIN);
+        fa.sum.bind(S);
+        fa.co.bind(CO);
+
+    }
+
+    void stimuli() {
+
+        //instantiate values
+        A.write(1);
+        B.write(1);
+        CIN.write(1);
+
+        wait(10, SC_NS);
+
+        std::cout << "sum : " << S.read() << "   cout : "  << CO.read() << std::endl;
+
+
+        A.write(1);
+        B.write(0);
+        CIN.write(0);
+
+        wait(10, SC_NS);
+
+        std::cout << "sum : " << S.read() << "   cout : "  << CO.read() << std::endl;
+
+    }
+
+
+
+};
+
+int sc_main (int, char **) {
+
+
+    toplevel tp ("tp");
+
+    sc_start();
+
+
+
+    return 0;
+}
